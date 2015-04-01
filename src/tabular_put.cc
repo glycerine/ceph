6e0ff82fa7a51fd7830a6084d09f36ca964d3ccf
Noah Watkins <noahwatkins@gmail.com>
2015-04-01 12:39:38 -0700

    create table
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/tabular_put.cc
+++ src/tabular_put.cc
@@ -9,12 +9,57 @@
 #include <iostream>
 #include <random>
 #include <thread>
+#include <boost/uuid/uuid.hpp>            // uuid class
+#include <boost/uuid/uuid_generators.hpp> // generators
+#include <boost/uuid/uuid_io.hpp>         // streaming operators etc.
+#include "include/types.h"
 #include "include/rados/librados.hpp"
 #include "cls/tabular/cls_tabular_client.h"
+
+struct table_split {
+  std::string oid;
+
+  void encode(bufferlist& bl) const {
+    ENCODE_START(1, 1, bl);
+    ::encode(oid, bl);
+    ENCODE_FINISH(bl);
+  }
+
+  void decode(bufferlist::iterator& bl) {
+    DECODE_START(1, bl);
+    ::decode(oid, bl);
+    DECODE_FINISH(bl);
+  }
+};
+WRITE_CLASS_ENCODER(table_split)
+
+struct table_state {
+  int seq;
+  std::string unique_id;
+  std::vector<table_split> splits;
+
+  void encode(bufferlist& bl) const {
+    ENCODE_START(1, 1, bl);
+    ::encode(unique_id, bl);
+    ::encode(splits, bl);
+    ::encode(seq, bl);
+    ENCODE_FINISH(bl);
+  }
+
+  void decode(bufferlist::iterator& bl) {
+    DECODE_START(1, bl);
+    ::decode(unique_id, bl);
+    ::decode(splits, bl);
+    ::decode(seq, bl);
+    DECODE_FINISH(bl);
+  }
+};
+WRITE_CLASS_ENCODER(table_state)
 
 struct table {
   librados::IoCtx& ioctx;
   std::string head;
+  table_state state;
 
   /*
    * poll for new splits. this should be replaced by watch/notify.
@@ -23,12 +68,39 @@ struct table {
   void watch_func() {
     while (1) {
       sleep(1);
-      std::cout << "Asdf" << std::endl;
+
+      bufferlist bl;
+      int ret = ioctx.read(head, bl, 0, 0);
+      assert(ret > 0);
+
+      librados::bufferlist::iterator iter = bl.begin();
+
+      table_state new_state;
+      ::decode(new_state, iter);
+
+      if (state.seq == new_state.seq) {
+        // copy over
+        for (unsigned i = 0; i < new_state.splits.size(); i++) {
+          std::cout << "table " << head << ": split " << i << ": " << new_state.splits[i].oid << std::endl;
+        }
+      }
     }
   }
 
   table(librados::IoCtx& ioctx, std::string head) :
     ioctx(ioctx), head(head) {
+
+      bufferlist bl;
+      int ret = ioctx.read(head, bl, 0, 0);
+      assert(ret > 0);
+
+      librados::bufferlist::iterator iter = bl.begin();
+      ::decode(state, iter);
+
+      for (unsigned i = 0; i < state.splits.size(); i++) {
+        std::cout << "table " << head << ": split " << i << ": " << state.splits[i].oid << std::endl;
+      }
+
       watch = new std::thread(&table::watch_func, *this);
   }
 
@@ -83,9 +155,11 @@ static void usage(const char *e)
 int main(int argc, char **argv)
 {
   std::string pool, objname;
+  bool create = false;
+  bool splitter = false;
 
   while (1) {
-    int c = getopt(argc, argv, "p:o:");
+    int c = getopt(argc, argv, "p:o:cs");
     if (c == -1)
       break;
     switch (c) {
@@ -94,6 +168,12 @@ int main(int argc, char **argv)
         break;
       case 'o':
         objname = std::string(optarg);
+        break;
+      case 'c':
+        create = true;
+        break;
+      case 's':
+        splitter = true;
         break;
       default:
         usage(argv[0]);
@@ -109,10 +189,37 @@ int main(int argc, char **argv)
   librados::IoCtx ioctx;
   open_ioctx(pool, rados, ioctx);
 
+  if (create) {
+    boost::uuids::uuid uuid = boost::uuids::random_generator()();
+    std::stringstream uuid_ss;
+    uuid_ss << uuid;
+
+    ioctx.remove(objname);
+    ioctx.create(objname, true);
+
+    table_state s;
+    s.seq = 0;
+    s.unique_id = uuid_ss.str();
+
+    uuid = boost::uuids::random_generator()();
+    uuid_ss.str("");
+    uuid_ss << s.unique_id << "." << uuid;
+    table_split split;
+    split.oid = uuid_ss.str();
+    s.splits.push_back(split);
+
+    bufferlist bl;
+    ::encode(s, bl);
+    int ret = ioctx.write_full(objname, bl);
+    assert(ret == 0);
+
+    return 0;
+  }
+
   std::default_random_engine generator;
   std::uniform_int_distribution<uint64_t> distribution(0, 1ULL<<32);
 
-  table t(ioctx, "Asdf");
+  table t(ioctx, objname);
 
   while (1) {
     std::vector<std::string> entries;
